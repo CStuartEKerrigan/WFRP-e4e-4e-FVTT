c3c34d00f9fe1c1eb6face1eed8e82a49cc2768d
stukerrigan <stuart.kerrigan@gmail.com>
2020-06-06 23:03:31 +0100

    Version 3.0 contains a script for the custom item in this scenario.

---
+++ scripts/korden.js
@@ -0,0 +1,26 @@
+/*
+
+Script to facilitate extra traits for Korden's Hammer. Currently a chat card separately appears reminding you about these additional traits if you attack an actor called Daemon.
+
+*/
+
+console.log("Eye for an Eye Script Loaded");
+
+ Hooks.on("wfrp4e:preOpposedTestResult", (attacker, defender) => {
+	 
+	 if (attacker.testResult.weapon.name == "Korden's Hammer" && isDemon(defender)){
+		let chatData = {
+			user : game.user._id,
+			content : "<img style=\"float: left; padding-right: 5px; border: 0\" src=\"modules/wfrp4e-e4e/assets/images/icons/kordenshammer.png\" alt=\"Hammer\"><p>Korden's hammer is engraved with runes to ward off daemons. Against a foe such as this it also counts as having the <strong><a class=\"item-property\">Damaging</a></strong> and <strong><a class=\"item-property\">Penetrating</a></strong> Qualities</p>",
+			whisper : game.users.entities.filter(u => u.isGM).map(u => u._id)
+		};
+		ChatMessage.create(chatData,{});
+	 }
+	 
+ })
+
+function isDemon(defender){
+	console.log(defender);
+	console.log(defender.actor);
+	return defender.speaker.alias.includes("Daemon"); // TO DO: Check for daemonic trait.
+}
